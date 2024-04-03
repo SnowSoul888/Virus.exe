1a2938639ff03d08ddf8b3f9961fdc3c6f65e63a
SnowS0ul <106284565+SnowSoul888@users.noreply.github.com>
2024-04-03 13:49:30 -0300

    Add files via upload

---
+++ Projeto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+class Program
+{
+    static void Main()
+    {
+        string explorerPath = Environment.GetEnvironmentVariable("windir") + @"\explorer.exe";
+        int numero = 5;
+        Console.WriteLine("Esta é uma mensagem de alerta!");
+        Console.ReadLine();
+        Console.WriteLine("Va olhar o lado de fora de sua casa!");
+        Console.ReadLine();
+        Console.WriteLine("Va olhar o lado de fora de sua casa!");
+        Console.ReadLine();
+        for(int i = 0; i < numero; i++)
+        {
+            // Abre o Windows Explorer no diretório especificado
+            Process.Start(explorerPath);
+            if(i == numero)
+            {
+                Process.Start("shutdown", "/s");
+            }
+        }
+    }
+}
